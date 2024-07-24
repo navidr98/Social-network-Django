5bfe16829a44b6f87a275b5c8521dae81e47bcb4
Navid_Rahmani <navidrahmani770@gmail.com>
2024-07-24 19:10:13 +0330

    validate register form fields

--- social/accounts/forms.py
+++ social/accounts/forms.py
@@ -1,7 +1,24 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class UserRegistrationForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        user = User.objects.filter(username=username).exists()
+        if user:
+            raise ValidationError('this username already exists')
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        user = User.objects.filter(email=email).exists()
+        if user:
+            raise ValidationError('this email already exits')
+        return email
