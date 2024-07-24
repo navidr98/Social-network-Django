a9aa55bdada0e735d2ed36c020cdb5a44c6170fb
Navid_Rahmani <navidrahmani770@gmail.com>
2024-07-24 16:38:21 +0330

    applying register view

--- social/accounts/views.py
+++ social/accounts/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .forms import UserRegistrationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+
 
 class RegisterView(View):
+
+    form_class = UserRegistrationForm
+    template_name = 'accounts/register.html'
     def get(self, request):
-        form = UserRegistrationForm
-        return render(request, 'accounts/register.html',context={'form':form})
+        form = self.form_class()
+        return render(request, self.template_name, context={'form':form})
 
     def post(self, request):
-        pass
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(cd['username'], cd['email'], cd['password'])
+            messages.success(request, 'you registered successfully', 'success')
+            return redirect('home:home')
+        return render(request, self.template_name, context={'form': form})
+
+
