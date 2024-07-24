41db7af1e729a8ed8d42bdce619f2940f5815ce8
Navid_Rahmani <navidrahmani770@gmail.com>
2024-07-24 15:55:46 +0330

    add namespace

--- social/accounts/urls.py
+++ social/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'accounts'
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='user_register')
 ]
--- social/social/urls.py
+++ social/social/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls')),
-    path('accounts/', include('accounts.urls')),
+    path('', include('home.urls', namespace='home')),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
 ]
