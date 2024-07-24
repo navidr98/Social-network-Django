b57fbebf8ad745d2109d1e2d9798eb07d63499db
Navid_Rahmani <navidrahmani770@gmail.com>
2024-07-24 20:29:16 +0330

    add confirm password

--- social/accounts/forms.py
+++ social/accounts/forms.py
@@ -6,7 +6,8 @@ from django.core.exceptions import ValidationError
 class UserRegistrationForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password = forms.CharField(label='password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password2 = forms.CharField(label='confirm password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
 
     def clean_username(self):
@@ -22,3 +23,11 @@ class UserRegistrationForm(forms.Form):
         if user:
             raise ValidationError('this email already exits')
         return email
+
+    def clean(self):
+        cd = super().clean()
+        p1 = cd.get('password')
+        p2 = cd.get('password2')
+        if p1 and p2 and p1 != p2 :
+            raise ValidationError('password doenst match')
+        
