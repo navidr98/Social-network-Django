b4c697cc93d8b63d1a886f97763d962c54e42578
Navid_Rahmani <navidrahmani770@gmail.com>
2024-07-25 16:19:15 +0330

    customizing PostAdmin panel

--- social/home/admin.py
+++ social/home/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('user', 'slug', 'updated')
+    search_fields = ('slug', )
+    list_filter = ('updated', )
+    prepopulated_fields = {'slug':('body',)}
+    raw_id_fields = ('user', )
+
+
+# admin.site.register(Post, PostAdmin)
+
