619ced93dfad77c9e71477898e53a19427668155
Navid_Rahmani <navidrahmani770@gmail.com>
2024-07-25 12:33:31 +0330

    add login mixin for limiting access

--- social/accounts/views.py
+++ social/accounts/views.py
@@ -4,7 +4,7 @@ from .forms import UserRegistrationForm , UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login ,logout
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class UserRegisterView(View):
 
@@ -57,7 +57,7 @@ class UserLoginView(View):
             messages.error(request, 'username or password is wrong', 'warning')
         return render(request, self.template_name, {'form':form})
 
-class UserLogoutView(View):
+class UserLogoutView(LoginRequiredMixin, View):
     def get(self, request):
         logout(request)
         messages.success(request, 'logged out successfully', 'success')
