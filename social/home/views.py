12fa1a192956e46db5f7fc2babbae10503eff5cb
Navid_Rahmani <navidrahmani770@gmail.com>
2024-07-26 18:08:49 +0330

    add delete post option

--- social/home/views.py
+++ social/home/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+
 
 class HomeView(View):
     def get(self, request):
@@ -14,3 +17,14 @@ class PostDetailView(View):
     def get(self, request, post_id, post_slug):
         post = Post.objects.get(pk=post_id, slug=post_slug)
         return render(request, 'home/detail.html', {'post':post})
+
+
+class PostDeleteView(LoginRequiredMixin, View):
+    def get(self, request, post_id):
+        post = Post.objects.get(pk=post_id)
+        if post.user.id == request.user.id:
+            post.delete()
+            messages.success(request, 'your post has been deleted', 'success')
+        else:
+            messages.error(request, 'you cant delete this post', 'danger')
+        return redirect('home:home')
