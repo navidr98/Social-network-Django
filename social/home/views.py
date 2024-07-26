a7b97c5079931f495f0a5879dfffba0ef8892278
Navid_Rahmani <navidrahmani770@gmail.com>
2024-07-26 21:14:09 +0330

    use setup method in update view

--- social/home/views.py
+++ social/home/views.py
@@ -31,24 +31,30 @@ class PostDeleteView(LoginRequiredMixin, View):
             messages.error(request, 'you cant delete this post', 'danger')
         return redirect('home:home')
 
+
 class PostUpdateView(LoginRequiredMixin, View):
 
     form_class = PostUpdateForm
 
+    def setup(self, request, *args, **kwargs):
+        self.post_instance = Post.objects.get(pk=kwargs['post_id'])
+        return super().setup(request, *args, **kwargs)
+
+
     def dispatch(self, request, *args, **kwargs):
-        post = Post.objects.get(pk=kwargs['post_id'])
+        post = self.post_instance
         if not post.user.id == request.user.id:
             messages.error(request, 'you have not access to update this post', 'danger')
             return redirect('home:home')
         return super().dispatch(request, *args, **kwargs)
 
-    def get(self, request, post_id):
-        post = Post.objects.get(pk=post_id)
+    def get(self, request, *args, **kwargs):
+        post = self.post_instance
         form = self.form_class(instance=post)
         return render(request, 'home/update.html', {'form':form})
 
     def post(self, request, post_id):
-        post = Post.objects.get(pk=post_id)
+        post = self.post_instance
         form = self.form_class(request.POST, instance=post)
         if form.is_valid():
             new_post = form.save(commit=False)
