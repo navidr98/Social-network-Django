33fa06bbe76939f7f8637d83dd58c3c1b168b422
Navid_Rahmani <navidrahmani770@gmail.com>
2024-07-27 19:20:55 +0330

    add follow and unfollow option

--- social/accounts/models.py
+++ social/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+class Relation(models.Model):
+    from_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
+    to_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+    created = models.DateTimeField(auto_now_add=True)
 
-# Create your models here.
+    def __str__(self):
+        return f'{self.from_user} following {self.to_user}'
